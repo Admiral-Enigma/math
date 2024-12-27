3f80f9eb907f1c4878f21b294ee2861e6b775aa0
FuTTiiZ <benjamin.b.soerensen@gmail.com>
2024-12-27 21:44:52 +0100

    update congruence

--- benny/congruences.ts
+++ benny/congruences.ts
@@ -7,12 +7,25 @@ class CongruenceSystem {
     this.vName = vName
   }
 
+  isSolution(n: number) {
+    return this.system.every(x => eval(x.replaceAll(this.vName, n.toString())))
+  }
+
   hasSolutionInRange(n: number, m: number) {
     for (let i = n; i < m + 1; i++) {
       if (this.system.every(x => eval(x.replaceAll(this.vName, i.toString()))))
         return true
     }
     return false
+  }
+
+  countSolutionsInRange(n: number, m: number) {
+    const v: number[] = []
+    for (let i = n; i < m + 1; i++) {
+      if (this.system.every(x => eval(x.replaceAll(this.vName, i.toString()))))
+        v.push(i)
+    }
+    return v.length
   }
 }
 
