f90bbc8835537b3327507af97e5e054563dd891d
FuTTiiZ <benjamin.b.soerensen@gmail.com>
2024-12-28 16:24:03 +0100

    added group problem solvers

--- benny/combinatorics.ts
+++ benny/combinatorics.ts
@@ -7,3 +7,17 @@ export const C = (n: number, r: number) =>
 
 // how many balls of a colors to guarantee b of same color?
 export const pigenhole = (a: number, b: number) => (b - 1) * a + 1
+
+// how many ways to put a people in b equally sized (unlabeled) groups
+export const equalUnlabeledGroups = (a: number, b: number) => {
+  if (a % b !== 0) throw Error('a must be divisible by b')
+  const n = a / b
+  return factorial(a) / (factorial(n) ** b * factorial(b))
+}
+
+// how many ways to put a people in b equally sized (labeled) groups
+export const equalLabeledGroups = (a: number, b: number) => {
+  if (a % b !== 0) throw Error('a must be divisible by b')
+  const n = a / b
+  return factorial(a) / factorial(n) ** b
+}
